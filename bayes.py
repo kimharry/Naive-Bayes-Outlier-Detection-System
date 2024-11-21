ea7881ddeb82f5c565900ffe6fb356a485601493
kimharry <kimharry0610@gmail.com>
2024-11-21 12:41:28 +0900

    datasets and skeleton code pasted

---
+++ bayes.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import argparse
+import logging
+
+def training(instances, labels):
+    pass
+
+def predict(instance, parameters):
+    pass
+
+def report(predictions, answers):
+    if len(predictions) != len(answers):
+        logging.error("The lengths of two arguments should be same")
+        sys.exit(1)
+
+    # accuracy
+    correct = 0
+    for idx in range(len(predictions)):
+        if predictions[idx] == answers[idx]:
+            correct += 1
+    accuracy = round(correct / len(answers), 2) * 100
+
+    # precision
+    tp = 0
+    fp = 0
+    for idx in range(len(predictions)):
+        if predictions[idx] == 1:
+            if answers[idx] == 1:
+                tp += 1
+            else:
+                fp += 1
+    precision = round(tp / (tp + fp), 2) * 100
+
+    # recall
+    tp = 0
+    fn = 0
+    for idx in range(len(answers)):
+        if answers[idx] == 1:
+            if predictions[idx] == 1:
+                tp += 1
+            else:
+                fn += 1
+    recall = round(tp / (tp + fn), 2) * 100
+
+    logging.info("accuracy: {}%".format(accuracy))
+    logging.info("precision: {}%".format(precision))
+    logging.info("recall: {}%".format(recall))
+
+def load_raw_data(fname):
+    instances = []
+    labels = []
+    with open(fname, "r") as f:
+        f.readline()
+        for line in f:
+            tmp = line.strip().split(", ")
+            tmp[1] = float(tmp[1])
+            tmp[2] = float(tmp[2])
+            tmp[3] = float(tmp[3])
+            tmp[4] = float(tmp[4])
+            tmp[5] = int(tmp[5])
+            tmp[6] = int(tmp[6])
+            tmp[7] = float(tmp[7])
+            tmp[8] = int(tmp[8])
+            instances.append(tmp[:-1])
+            labels.append(tmp[-1])
+    return instances, labels
+
+def run(train_file, test_file):
+    # training phase
+    instances, labels = load_raw_data(train_file)
+    logging.debug("instances: {}".format(instances))
+    logging.debug("labels: {}".format(labels))
+    parameters = training(instances, labels)
+
+    # testing phase
+    instances, labels = load_raw_data(test_file)
+    predictions = []
+    for instance in instances:
+        result = predict(instance, parameters)
+
+        if result not in [0, 1]:
+            logging.error("The result must be either 0 or 1")
+            sys.exit(1)
+
+        predictions.append(result)
+    
+    # report
+    report(predictions, labels)
+
+def command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--training", required=True, metavar="<file path to the training dataset>", help="File path of the training dataset", default="training.csv")
+    parser.add_argument("-u", "--testing", required=True, metavar="<file path to the testing dataset>", help="File path of the testing dataset", default="testing.csv")
+    parser.add_argument("-l", "--log", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = command_line_args()
+    logging.basicConfig(level=args.log)
+
+    if not os.path.exists(args.training):
+        logging.error("The training dataset does not exist: {}".format(args.training))
+        sys.exit(1)
+
+    if not os.path.exists(args.testing):
+        logging.error("The testing dataset does not exist: {}".format(args.testing))
+        sys.exit(1)
+
+    run(args.training, args.testing)
+
+if __name__ == "__main__":
+    main()
